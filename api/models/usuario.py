bee0e3a191b609bae849b64be4e4579fcfae7552
rodrichc <rodrichc1@gmail.com>
2026-04-26 21:49:12 -0300

    fix: corrige que el email sea unico al registrarse

--- api/models/usuario.py
+++ api/models/usuario.py
@@ -1,6 +1,9 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
+    email = models.EmailField(unique=True)
+
     class Meta:
         app_label = "api"
 
